2e255b0ae08aa8d54bf79473f2f50d9738e1ac7c
Tygger <tygger.r@gmail.com>
2019-10-20 18:17:08 +0200

    Core OS command handling

--- MothershipConsole/MothershipConsole.cs
+++ MothershipConsole/MothershipConsole.cs
@@ -13,6 +13,8 @@ namespace MothershipConsole
         private static IEnumerable<ListApp> Apps;
         private static IEnumerable<Command> Commands;
         
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Loading main Mothership terminal...");
@@ -20,32 +22,102 @@ namespace MothershipConsole
 
             // get apps
             Apps = GetImplementedApps();
+
+            //get commands
+            Commands = GetCommands();
+
+            //infinite command loop
+            while (true) {
+                HandleCommand();
+            }
+        }
+
+
+
+
+
+        #region Commands
+        // COMMAND DEFINITIONS
+
+        [Command("help")]
+        [CommandHelp("Lists all available commands")]
+        public static bool HelpCommand(string arg = "")
+        {
+            Console.WriteLine("Available Commands:");
+            foreach (var command in Commands)
+            {
+                Console.WriteLine(" - '" + command.Name + "' (" + command.Help + ")");
+            }
+            return true;
+        }
+
+        [Command("apps")]
+        [CommandHelp("Lists all installed apps")]
+        public static bool AppCommand(string arg = "")
+        {
             Console.WriteLine("Available Apps:");
             foreach (var app in Apps)
             {
                 Console.WriteLine(" - " + app.Name);
             }
-
-            //get commands
-            Commands = GetCommands();
-            Console.WriteLine("Available Commands:");
-            foreach (var command in Commands)
-            {
-                Console.WriteLine(" - " + command.Name + " (" + command.Help + ")");
-            }
-
-            Console.WriteLine("");
-
-            //infinite command loop
-            while (true) {
-                Console.WriteLine("press any key to run Testing app");
-                Console.ReadKey(true);
-                Console.WriteLine();
-                var testing = new Testing();
-                testing.Run();
-                Console.WriteLine();
-            }
+            return true;
         }
+
+        [Command("test")]
+        [CommandHelp("Yells and does nothing")]
+        public static bool TestCommand(string arg = "")
+        {
+            Console.WriteLine("I am soooooooooo testy!");
+
+            return true;
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+        #region Other Methods
+        // OTHER METHODS
+
+        static bool HandleCommand()
+        {
+            Console.WriteLine();
+            Console.Write("$-Network/Ship/OS> ");
+
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(new string[] { " " }, 2, StringSplitOptions.None);
+            string inputCommand = words[0];
+            string inputArgs = words.Length > 1 ? words[1] : null;
+
+            Command command = Commands.Where(c => c.Name == inputCommand).FirstOrDefault();
+
+            if (command == null)
+            {
+                Console.WriteLine("Command '" + inputCommand + "' not found");
+                Console.WriteLine("Use 'help' for available commands");
+
+                return false;
+            }
+
+            if (command.Method.GetParameters().FirstOrDefault()?.ParameterType == typeof(string))
+            {
+                command.Method.Invoke(null, new object[] { inputArgs });
+            }
+            else
+            {
+                command.Method.Invoke(null, new object[] { } );
+            }
+
+            return true;
+        }
+
 
         static IEnumerable<ListApp> GetImplementedApps()
         {
@@ -70,10 +142,13 @@ namespace MothershipConsole
 
             var commands = typeof(MothershipConsole).GetMethods()
                       .Where(m => m.GetCustomAttributes(typeof(CommandAttribute), false).Length > 0)
-                      .Select(m => new Command() {
-                                                    Name = ((CommandAttribute)(m.GetCustomAttributes(typeof(CommandAttribute), true).First())).Name
-                                                  , Help = ((CommandHelpAttribute)(m.GetCustomAttributes(typeof(CommandHelpAttribute), true).First())).Help
-                                                  , Method = m
+                      .Select(m => new Command()
+                      {
+                          Name = ((CommandAttribute)(m.GetCustomAttributes(typeof(CommandAttribute), true).First())).Name
+                                                  ,
+                          Help = ((CommandHelpAttribute)(m.GetCustomAttributes(typeof(CommandHelpAttribute), true).First())).Help
+                                                  ,
+                          Method = m
                       })
                       .ToList()
                       ;
@@ -81,15 +156,6 @@ namespace MothershipConsole
             return commands;
         }
 
-
-
-        [Command("test")]
-        [CommandHelp("Yells and does nothing")]
-        public static bool TestCommand(string arg)
-        {
-            Console.WriteLine("I am soooooooooo testy!");
-
-            return true;
-        }
+        #endregion
     }
 }
