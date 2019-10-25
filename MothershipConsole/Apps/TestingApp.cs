2187de39c35f19a1391963b4424f4f95e555759c
Tygger <tygger.r@gmail.com>
2019-10-25 02:33:24 +0200

    use Figgle for Figlet fonts

--- MothershipConsole/Apps/TestingApp.cs
+++ MothershipConsole/Apps/TestingApp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using Colorful;
+using Figgle;
 using Console = Colorful.Console;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,12 @@ namespace MothershipConsole.Apps
     [App("test_app")]
     class TestingApp : App
     {
-        protected override void WriteAppTitle() { Console.WriteAscii("THE TESTING APP!", Color.White); }
+        protected override void WriteAppTitle() 
+        {
+            // choose font on http://www.figlet.org/examples.html
+            var font = FiggleFonts.Epic ;
+            Console.WriteLine(font.Render("Testing App"));
+        }
         protected override string ReadPrefix { get { return "[TestingApp]->"; } }
         protected override Color BackGroundColor { get { return Color.DarkMagenta; } }
         protected override Color TextColor { get { return Color.White; } }
